442f73356b37437ae9371de0977ac6827583b76d
mokus <spalia@web.de>
2013-05-21 16:44:11 +0300

    add search bar

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,12 @@
 class Word < ActiveRecord::Base
   attr_accessible :english, :polish
+
+	def self.search(search)
+	  if search
+	    find(:all, :conditions => ['english LIKE ? OR polish LIKE ?', "%#{search}%", "%#{search}%"])
+	  else
+	    find(:all)
+	  end
+	end
+
 end
